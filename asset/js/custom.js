47ee53f8414ebac1ad9edf240705e9acd741501d
devshisir <contact.shisirimran@gmail.com>
2020-10-23 06:27:11 -0700

    Footer Component

---
+++ asset/js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var Fullwindow = $(window);
+    var page = $('html, body');
+
+
+
+    console.log('i am ready');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
